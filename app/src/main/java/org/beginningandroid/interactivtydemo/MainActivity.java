9f88e49cf737af8be03878ff6145637ca02c5887
mc227 <markcostales.mark@gmail.com>
2023-01-31 23:00:42 -0500

    before making the functionality work

---
+++ app/src/main/java/org/beginningandroid/interactivtydemo/MainActivity.java
@@ -0,0 +1,16 @@
+package org.beginningandroid.interactivtydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
